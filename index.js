92c21f9d0d5fe6d66185f846e8c4d2795489c639
carolaresch2203 <carola.22r@gmail.com>
2024-03-10 19:11:19 -0300

    Funcion desencriptar()

--- index.js
+++ index.js
@@ -23,3 +23,28 @@ function encriptar() {
       swal("Ooops!", "Debes ingresar un texto", "warning");
     }
   }
+
+  function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");   
+
+    if (texto.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto Desecriptado con exito!!";
+        parrafo.textContent = "";
+        muñeco.src = "./resources/desencriptado.jpg";
+    }else{
+      muñeco.src = "./resources/Muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning"); 
+    } 
+  }
